c6a6e585935751e1b92b0380cfea502c4334a9b7
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-15 09:47:16 -0700

    Refactor DB code to run asycn

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -19,10 +20,10 @@ namespace eCommerceSite.Controllers
 		/// <summary>
 		/// Displays a view that lists all products
 		/// </summary>
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
 			// Get all products from database
-			List<Product> products = _context.Products.ToList();
+			List<Product> products = await _context.Products.ToListAsync();
 
 			// Send list of products to view to be displayed
 			return View(products);
@@ -35,13 +36,13 @@ namespace eCommerceSite.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult Add(Product p)
+		public async Task<IActionResult> Add(Product p)
 		{
 			if (ModelState.IsValid)
 			{
 				// Add to DB
 				_context.Products.Add(p);
-				_context.SaveChanges();
+				await _context.SaveChangesAsync();
 
 				TempData["Message"] = $"{p.Title} was added successfully";
 
