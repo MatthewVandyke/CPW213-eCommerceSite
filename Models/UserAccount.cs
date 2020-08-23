232e1af8f0a5f0537ff268c0189fb6d27b85c58a
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-22 23:48:36 -0700

    Created a UserAccount model

---
+++ Models/UserAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+	public class UserAccount
+	{
+		[Key]
+		public int UserId { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		public DateTime? DateOfBirth { get; set; }
+	}
+}
