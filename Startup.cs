d4babc41f4281911d4cfefd45e5f8439597d9f22
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-29 01:54:37 -0700

    Enable sessions

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,14 @@ namespace eCommerceSite
 
 			services.AddDbContext<ProductContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddDistributedMemoryCache();
+
+			services.AddSession(options =>
+			{
+				options.IdleTimeout = TimeSpan.FromMinutes(30);
+				options.Cookie.IsEssential = true;
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +58,8 @@ namespace eCommerceSite
 			app.UseRouting();
 
 			app.UseAuthorization();
+
+			app.UseSession();
 
 			app.UseEndpoints(endpoints =>
 			{
