f3e93626b1b1461b304655336946f632ea63cad5
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-30 11:53:07 -0700

    Add shopping cart link to nav and add to cart button for each product

---
+++ Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Add(int id) // Id of the product to add
+		{
+			// Get prod from database
+			// Add prodcut to cart cookie
+
+			// Redirect
+			return View();
+		}
+
+		public IActionResult Summary()
+		{
+			// Display all products in cart cookie
+			return View();
+		}
+	}
+}
