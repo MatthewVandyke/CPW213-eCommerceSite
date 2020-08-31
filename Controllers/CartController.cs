26fad8d81dde5353c550eccfdd25703f8c41a713
Matthew VanDyke <48613756+MatthewVandyke@users.noreply.github.com>
2020-08-31 03:31:47 -0700

    Issue#26 shopping cart (#37)
    * Add shopping cart icon to nav
    
    * Add shopping cart link to nav and add to cart button for each product
    
    * Made edit and delete more readable
    
    * A single product can now be added to cart
    
    * Able to store multiple items in shopping cart
    
    * Created basic summary page. Does not work with empy cookie
    
    * Refactored cart controller
    
    * Added a message for an empty cart

---
+++ Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace eCommerceSite.Controllers
+{
+	public class CartController : Controller
+	{
+		private readonly ProductContext _context;
+
+		public CartController(ProductContext context)
+		{
+			_context = context;
+		}
+
+		/// <summary>
+		/// Adds a product to the shopping cart
+		/// </summary>
+		/// <param name="id">The id of the product to add</param>
+		public async Task<IActionResult> Add(int id)
+		{
+			const string CartCookie = "CartCookie";
+
+			// Get prod from database
+			Product p =
+				await ProductDb.GetProductAsync(_context, id);
+
+			CookieHelper.AddProductToCart(HttpContext, p);
+
+			// Redirect
+			return RedirectToAction("Index", "Product");
+		}
+
+		public IActionResult Summary()
+		{
+			List<Product> cartProducts = CookieHelper.GetCartProducts(HttpContext);
+			return View(cartProducts);
+		}
+	}
+}
